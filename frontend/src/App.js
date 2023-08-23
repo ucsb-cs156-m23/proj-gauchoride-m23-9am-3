81952a39a761de45c4e0478ec2eaff225ebc3b20
lawrencewang30 <jiabin2003@gmail.com>
2023-08-23 15:32:07 -0700

    lw - 1st attempt at fixing conflicting file with App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,22 +56,6 @@ function App() {
           hasRole(currentUser, "ROLE_USER")
         }
         <Route exact path="/*" element={<PageNotFound />} />
-        {
-          (hasRole(currentUser, "ROLE_DRIVER") || hasRole(currentUser, "ROLE_USER")) && (
-            <>
-              <Route exact path="/shift" element={<ShiftIndexPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_ADMIN") && (
-            <>
-              <Route exact path="/shift/edit/:id" element={<ShiftEditPage />} />
-              <Route exact path="/shift/create" element={<ShiftCreatePage />} />
-            </>
-          )
-        }
-
       </Routes>
     </BrowserRouter>
   );
