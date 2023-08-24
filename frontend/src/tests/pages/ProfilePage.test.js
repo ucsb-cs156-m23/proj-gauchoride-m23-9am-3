5688c7831e7f869ce7250c5a30fe8464f27c04bb
Phill Conrad <phtcon@ucsb.edu>
2023-08-24 14:31:09 -0700

    pc - refactor axios mocks and reorder tests to avoid flaky test problem

--- frontend/src/tests/pages/ProfilePage.test.js
+++ frontend/src/tests/pages/ProfilePage.test.js
@@ -11,10 +11,42 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 describe("ProfilePage tests", () => {
 
     const queryClient = new QueryClient();
+    const axiosMock =new AxiosMockAdapter(axios);
+
+    beforeEach( () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+    });
+
+    test("the modal closes when handleClose is triggered", async () => {
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    
+        const { getByText, queryByText } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ProfilePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    
+        await waitFor(() => expect(getByText("Cellphone No.:")).toBeInTheDocument());
+    
+        const editButton = getByText('Edit');
+        fireEvent.click(editButton);
+    
+        expect(getByText('Update Cell Phone Number')).toBeInTheDocument();
+    
+        const closeButton = getByText('Close');
+        fireEvent.click(closeButton);
+    
+        await waitFor(() => {
+            expect(queryByText('Update Cell Phone Number')).not.toBeInTheDocument();
+        });    
+    }); 
 
     test("renders correctly for regular logged in user", async () => {
 
-        const axiosMock =new AxiosMockAdapter(axios);
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
@@ -32,7 +64,6 @@ describe("ProfilePage tests", () => {
 
     test("renders correctly for admin user", async () => {
 
-        const axiosMock =new AxiosMockAdapter(axios);
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
@@ -60,7 +91,6 @@ describe("ProfilePage tests", () => {
 
     test("renders correctly for driver", async () => {
 
-        const axiosMock =new AxiosMockAdapter(axios);
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.driverOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
@@ -80,7 +110,6 @@ describe("ProfilePage tests", () => {
 
     test("renders correctly for rider", async () => {
 
-        const axiosMock =new AxiosMockAdapter(axios);
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.riderOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
@@ -100,7 +129,6 @@ describe("ProfilePage tests", () => {
 
     test("cellphone number is displayed", async () => {
 
-        const axiosMock =new AxiosMockAdapter(axios);
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
@@ -117,7 +145,6 @@ describe("ProfilePage tests", () => {
 
     test("cellphone number can be updated through the modal", async () => {
 
-        const axiosMock = new AxiosMockAdapter(axios);
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
         axiosMock.onPut("/api/userprofile/updatecellphone").reply(200, { success: true });
@@ -146,33 +173,7 @@ describe("ProfilePage tests", () => {
         await waitFor(() => expect(getByText('Cellphone No.: 1234567890')).toBeInTheDocument());
     });
 
-    test("the modal closes when handleClose is triggered", async () => {
-        const axiosMock = new AxiosMockAdapter(axios);
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    
-        const { getByText, queryByText } = render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <ProfilePage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-    
-        await waitFor(() => expect(getByText("Cellphone No.:")).toBeInTheDocument());
-    
-        const editButton = getByText('Edit');
-        fireEvent.click(editButton);
-    
-        expect(getByText('Update Cell Phone Number')).toBeInTheDocument();
-    
-        const closeButton = getByText('Close');
-        fireEvent.click(closeButton);
-    
-        await waitFor(() => {
-            expect(queryByText('Update Cell Phone Number')).not.toBeInTheDocument();
-        });    
-    });    
+       
 });
 
 
