7cbc98617ae4d63d87db4c3d028a20f5e2baf021
lawrencewang30 <jiabin2003@gmail.com>
2023-08-24 11:12:42 -0700

    lw-edited App.js to merge code into dokku, will change back after

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "main/pages/HomePage";
 import ProfilePage from "main/pages/ProfilePage";
 import AdminUsersPage from "main/pages/AdminUsersPage";
-import PageNotFound from "main/pages/PageNotFound";
-import PrivacyPolicy from "main/pages/PrivacyPolicy";
+
 
 import RideRequestCreatePage from "main/pages/Ride/RideRequestCreatePage";
 import RideRequestEditPage from "main/pages/Ride/RideRequestEditPage";
@@ -67,11 +66,6 @@ function App() {
             </>
           )
         }
-        {
-          hasRole(currentUser, "ROLE_USER")
-        }
-        <Route exact path="/*" element={<PageNotFound />} />
-        <Route exact path="/privacy" element={<PrivacyPolicy />} />
       </Routes>
     </BrowserRouter>
   );
